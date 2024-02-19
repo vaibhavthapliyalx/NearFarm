b83391de2d1661f574b484c6a0c32c9efabd5746
Vaibhav Thapliyal <vaibhavthapliyal@Vaibhavs-MBP.broadband>
2024-02-19 23:29:32 +0000

    Fixed unnecessary NextResponse from db connection api

--- app/api/connection/database/route.ts
+++ app/api/connection/database/route.ts
@@ -1,7 +1,7 @@
 import { pingDB } from "@/lib/database";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextResponse) {
+export async function GET(request: NextRequest) {
   try {
     pingDB();
     return NextResponse.json({status: 200, message: "Database connection is functional"},
