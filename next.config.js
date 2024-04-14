bc92ae66e142e41035b622475505f03c1ea115ee
Vaibhav Thapliyal <vaibhavthapliyal@Vaibhavs-MBP.broadband>
2024-04-14 22:55:04 +0100

    Disabled ESlint during production build

--- next.config.js
+++ next.config.js
@@ -6,6 +6,11 @@ const nextConfig = {
       'www.bigbasket.com'
     ],
   },
+  // Here we are disabling eslint validation during build,
+  // because it is not required and it will save some time during build.
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig;
